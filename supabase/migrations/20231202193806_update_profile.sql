122308b6e7f3d4a83ac600877be2bbf0172775dc
bmuday <bmuday@live.fr>
2023-12-02 20:40:01 +0100

    update profile

---
+++ supabase/migrations/20231202193806_update_profile.sql
@@ -0,0 +1,7 @@
+alter table "public"."profile" alter column "age" drop not null;
+
+alter table "public"."profile" alter column "location" drop not null;
+
+alter table "public"."profile" alter column "male" drop not null;
+
+
